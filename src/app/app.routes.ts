ba0d6ae98f8cb0fd3393c52f9c8b36397c4ba8ba
artur-mrozek <artur.mrozek1@gmail.com>
2025-06-01 12:04:20 +0200

    mars weather page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component'
 import { MarsPhotosComponent } from './features/mars-photo/components/component';
+import { MarsWeatherPageComponent } from './pages/mars-weather-page/mars-weather-page.component';
 
 export const routes: Routes = [
     {path: '', component: HomePageComponent },
-    {path: 'mars-weather', component: HomePageComponent },
-    {path: 'mars-photos', component: MarsPhotosComponent }
+    {path: 'mars-photos', component: MarsPhotosComponent },
+    {path: 'mars-weather', component: MarsWeatherPageComponent}
 ];
---
+++ src/app/pages/mars-weather-page/mars-weather-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mars-weather-page',
+  imports: [],
+  templateUrl: './mars-weather-page.component.html',
+  styleUrl: './mars-weather-page.component.scss'
+})
+export class MarsWeatherPageComponent {
+
+}
